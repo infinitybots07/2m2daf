922b23a006bfbdd699c6cf8934712e52bc5f6fce
NIHUU <106787129+NIHUU@users.noreply.github.com>
2022-08-08 16:26:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/4kcinemas/Netflix-Bot.git /Netflix-Bot
+  git clone https://github.com/NIHUU/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Netflix-Bot
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /Netflix-Bot
+cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "sᴛᴀʀᴛɪɴɢ 𝗗𝗞 𝗕𝗢𝗧𝘅........"
 python3 bot.py
