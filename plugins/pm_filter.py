8339a3dda2d5e9f419d42d5fec02d60bd9ef13a9
NIHUU <106787129+NIHUU@users.noreply.github.com>
2022-08-21 14:50:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -888,8 +888,9 @@ async def advantage_spell_check_1_(msg):
         await asyncio.sleep(259200)
         await msg.delete()
         await ms.delete()
-    if not imdb:
-        ni = await msg.reply_photo(photo="https://telegra.ph/file/90049c7aa5b86b101a8d7.jpg", 
+    if not imdb['poster']:
+        ni = await msg.reply_photo(
+            photo="https://telegra.ph/file/90049c7aa5b86b101a8d7.jpg", 
             caption=script.IMDB_MOVIE_2.format(query=search, title=imdb['title'], rating=imdb['rating'], genres=imdb['genres'], year=imdb['year'], runtime=imdb['runtime'], language=imdb['languages'], group=msg.chat.title, url="https://t.me/CL_UPDATE", short=imdb['plot']), 
             reply_markup=reply_markup
         )
