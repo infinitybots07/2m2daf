f00ac535ce9b65af56009cdd6d9c2f21bcdaed44
NIHUU <106787129+NIHUU@users.noreply.github.com>
2022-08-21 14:59:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -881,22 +881,13 @@ async def advantage_spell_check_1_(msg):
     imdb=await get_poster(search)
     if imdb and imdb.get('poster'):
         ms = await msg.reply_photo(
-            photo=imdb.get('poster'), 
+            photo=imdb.get('poster') if imdb.get('poster') else "https://telegra.ph/file/90049c7aa5b86b101a8d7.jpg",
             caption=script.IMDB_MOVIE_2.format(query=search, title=imdb.get('title'), rating=imdb.get('rating'), genres=imdb.get('genres'), year=imdb.get('year'), runtime=imdb.get('runtime'), language=imdb.get('languages'), group=msg.chat.title, url="https://t.me/CL_UPDATE", short=imdb['plot']), 
             reply_markup=reply_markup
         ) 
         await asyncio.sleep(259200)
         await msg.delete()
         await ms.delete()
-    if not imdb:
-        ni = await msg.reply_photo(
-            photo="https://telegra.ph/file/90049c7aa5b86b101a8d7.jpg", 
-            caption=script.IMDB_MOVIE_2.format(query=search, title=imdb['title'], rating=imdb['rating'], genres=imdb['genres'], year=imdb['year'], runtime=imdb['runtime'], language=imdb['languages'], group=msg.chat.title, url="https://t.me/CL_UPDATE", short=imdb['plot']), 
-            reply_markup=reply_markup
-        )
-        await asyncio.sleep(259200)
-        await msg.delete()
-        await ni.delete()
     else:
         buttons = [[
             InlineKeyboardButton('🍁 Rᴇᴀsᴏɴ', "reason"),
