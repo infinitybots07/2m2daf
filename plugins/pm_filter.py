253d99b82f2266f740095413f1857bb66b6743e4
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-09-04 10:28:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -110,8 +110,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⊹ {get_size(file.file_size)} ‣ {file.file_name}", callback_data=f'{pre}#{file.file_id}#{query.from_user.id}'
-                ),
+                    text=f"⊹ {get_size(file.file_size)} ‣ {file.file_name}", url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}"                ),
             ]
             for file in files
         ]
@@ -813,7 +812,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⊹ {get_size(file.file_size)} ‣ {file.file_name}", callback_data=f'{pre}#{file.file_id}#{msg.from_user.id if msg.from_user is not None else 0}'
+                    text=f"⊹ {get_size(file.file_size)} ‣ {file.file_name}", url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}"
                 ),
             ]
             for file in files
