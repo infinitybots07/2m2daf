fc9fcd902019437a36a88cd011aad21f5a37bc89
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-09-04 18:19:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -29,7 +29,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Aᴅᴅ Bᴏᴛ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ➕', url='http://t.me/CL_FILTER_BOT?startgroup=true')
         ]]     
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
+        await client.send_text(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/Aadhi000/Ajax-Extra-Features/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
