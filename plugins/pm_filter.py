ad30a2f71288bb43f735131e7088ba90c8ff056e
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-09-06 10:09:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -44,32 +44,51 @@ async def give_filter(client,message):
             reply_text, btn, alert, fileid = await find_filter(group_id, keyword)
 
             if reply_text:
-                reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
-
+                f2 = reply_text = reply_text.replace("\\n", "\n").replace("\\t", "\t")
+                await asyncio.sleep(12)
+                await message.delete()
+                await f2.delete()
+                del message, f2
             if btn is not None:
                 try:
                     if fileid == "None":
                         if btn == "[]":
-                            await message.reply_text(reply_text, disable_web_page_preview=True)
+                            f2 = await message.reply_text(reply_text, disable_web_page_preview=True)
+                            await asyncio.sleep(12)
+                            await message.delete()
+                            await f2.delete()
+                            del message, f2
                         else:
                             button = eval(btn)
-                            await message.reply_text(
+                            f2 = await message.reply_text(
                                 reply_text,
                                 disable_web_page_preview=True,
                                 reply_markup=InlineKeyboardMarkup(button)
                             )
+                            await asyncio.sleep(12)
+                            await message.delete()
+                            await f2.delete()
+                            del message, f2
                     elif btn == "[]":
-                        await message.reply_cached_media(
+                        f2 = await message.reply_cached_media(
                             fileid,
                             caption=reply_text or ""
                         )
+                        await asyncio.sleep(12)
+                        await message.delete()
+                        await f2.delete()
+                        del message, f2
                     else:
                         button = eval(btn) 
-                        await message.reply_cached_media(
+                        f2 = await message.reply_cached_media(
                             fileid,
                             caption=reply_text or "",
                             reply_markup=InlineKeyboardMarkup(button)
                         )
+                        await asyncio.sleep(12)
+                        await message.delete()
+                        await f2.delete()
+                        del message, f2
                 except Exception as e:
                     print(e)
                 break 
