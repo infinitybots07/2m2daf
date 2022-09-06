8fce5f1ccbf8047e4984bba32971b208222c6fe1
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-09-06 20:24:20 +0530

    Update filestore.py

--- plugins/filestore.py
+++ plugins/filestore.py
@@ -5,7 +5,6 @@ from pyromod import listen
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup 
 from pyrogram import Client, filters 
 from pyrogram.errors import PeerIdInvalid, MessageIdInvalid
-from bot.helpers import encode
 
 @Client.on_message(filters.command("batch") & filters.private & ~filters.bot, group=3) 
 async def batch(bot:Client, update:Message): 
