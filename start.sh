a41005a3a1a07b89d1cb3f570b1fe7ceb3440d56
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-09-14 22:20:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/enthada-myre/nouse /nouse
+  git clone https://github.com/enthada-myre/ThomasBotV3 /ThomasBotV3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /nouse
+  git clone $UPSTREAM_REPO /ThomasBotV3
 fi
-cd /nouse
+cd /ThomasBotV3
 pip3 install -U -r requirements.txt
 echo "sᴛᴀʀᴛɪɴɢ......."
 python3 bot.py
