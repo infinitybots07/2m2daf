ba636bab91622a2cb37ff8460aa7a2068239fc3c
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-10-16 11:25:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,9 +22,9 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
+            workers=100,
             plugins={"root": "plugins"},
-            sleep_threshold=5,
+            sleep_threshold=10,
         )
 
     async def start(self):
