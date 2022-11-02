a0486a29cc1d5c48c62866c2ae85831eeafbb90b
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-11-02 17:42:09 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -4,6 +4,14 @@ from info import API_ID, API_HASH
 
 clients = []
 
+async def start_(msg):
+    me = await msg.client.get_me()
+    return await msg.reply(f"Hello Im, @-{me.username}, running in cloneMode.")
+
+
+def load_handlers(bot):
+    bot.add_event_handler(start_, events.NewMessage(pattern="^[!/?]start$"))
+
 async def addBot(token):
     botID = token.split(":")[0]
     tgClient = Client(botID + "-0", API_ID, API_HASH)
@@ -38,7 +46,7 @@ async def get_text_content(message):
 
 @Client.on_message((filters.private | filters.group) & filters.command('clone'))
 async def clone(client, msg):
-  tok = await get_text_content
+  tok = await get_text_content(msg)
   if not tok:
     return await msg.reply("I Cᴏᴜʟᴅ Nᴏᴛ Fɪɴᴅ Aɴʏ Tᴏᴋᴇɴ Lɪᴋᴇ Tʜᴀᴛ")
   add = await addBot(tok)
