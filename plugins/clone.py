c165314c48782ce6298bf15ba46987f42417f3dc
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-11-02 08:10:07 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -1,10 +1,34 @@
 from pyrogram import Client, filters
+import os
 
 
+async def get_text_content(message):
+    """Returns the text content of a message."""
+    if message.reply_to_msg_id:
+        reply = await message.get_reply_message()
+        if reply.media:
+            if reply.document:
+                doc = await reply.download_media()
+                with open(doc, "r", errors="ignore") as f:
+                    u = f.read()
+                os.remove(doc)
+                return u
+            else:
+                return None
+        else:
+            return reply.text
+    else:
+        try:
+            return message.text.split(" ", 1)[1]
+        except:
+            return None
 
 @Client.on_message(filters.command(["clone"]))
 async def clone(client, msg):
   tok = await get_text_content
   if not tok:
     return await msg.reply("I Cᴏᴜʟᴅ Nᴏᴛ Fɪɴᴅ Aɴʏ Tᴏᴋᴇɴ Lɪᴋᴇ Tʜᴀᴛ")
+  add = await addbot(tok)
+  if add:
+    
   
