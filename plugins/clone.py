4ec64422b5c77b9542e59c71e1c38de40699e20a
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-11-02 22:22:49 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -11,7 +11,7 @@ async def start_(msg):
 
 
 def load_handlers(bot):
-    bot.add_handler(MessageHandler(start_, filters.command('start')))
+    
 
 async def addBot(msg):
     botID = msg.split(":")[0]
