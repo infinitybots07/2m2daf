1bc0c21fa6c41ed1ace8b6b2ef72e38cdcfb1346
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-11-03 08:15:10 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -21,10 +21,7 @@ async def addBot(msg):
     clients.append(tgClient)
     try:
         await Client.start(tgClient)
-    except Exception as err:
-        return str(err)
-    load_handlers(tgClient)
-    return ""
+    
 
 async def get_text_content(message):
     """Returns the text content of a message."""
