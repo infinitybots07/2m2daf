343844732190b0cab6724ccbed2ed3efdcc49419
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-11-03 08:37:17 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -53,7 +53,7 @@ async def clone(client, msg):
   tok = await get_text_content(msg)
   if not tok:
     return await msg.reply("I Cᴏᴜʟᴅ Nᴏᴛ Fɪɴᴅ Aɴʏ Tᴏᴋᴇɴ Lɪᴋᴇ Tʜᴀᴛ")
-  add = await addBot()
+  add = await addBot(msg)
   if add != "":
       return await msg.add(tok)
   return await msg.reply("connected")
