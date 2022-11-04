8e8c8cfe17035bc29ef3de766b86c4cd62e56c35
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-11-04 22:06:01 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -8,17 +8,17 @@ from info import API_ID, API_HASH
 @Client.on_message(filters.private & filters.command("clone"))
 async def clone(bot, msg: Message):
     chat = msg.chat
-    text = await msg.reply("Usage:\n\n /clone token")
+    text = await msg.reply("<b>Hᴇʏ Bʀᴏ Usᴇ Cᴏʀʀᴇᴄᴛ Mᴇᴛʜᴏᴅ\n\nEɢ : <code>/clone [bot token]</code></b>")
     cmd = msg.command
     phone = msg.command[1]
     try:
-        await text.edit("Booting Your Client")
+        await text.edit("<b>Tʀʏɪɴɢ Tᴏ Cᴏɴɴᴇᴄᴛ Yᴏᴜʀ Bᴏᴛ...</b>")
                    # change this Directry according to ur repo
         client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "Clone"})
         await client.start()
         user = await client.get_me()
-        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\nThanks for Cloning.")
+        await msg.reply(f"<b>Hᴇʏ Bʀᴏ Yᴏᴜ Bᴏᴛ Hᴀs Bᴇᴇɴ Sᴛᴀʀᴛᴇᴅ As @{user.username} ✅ \n\nAᴅᴅ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Eɴᴊᴏʏ.. 📣</b>")
         APP_USERNAME = user.username
-        await client.send_message(APP_USERNAME, "/start")
+        await Client.send_message(APP_USERNAME, "/start")
     except Exception as e:
-        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+        await msg.reply(f"**❌ ERROR:**\n\n`{str(e)}`\n\nIғ Hᴀᴠᴇ Aɴʏ Dᴏᴜʙᴛ Asᴋ Iɴ Sᴜᴘᴘᴏʀᴛ ❗")
