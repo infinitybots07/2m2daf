294a27e93df573172a6a383c584c027b00da7b23
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-11-04 10:35:41 +0530

    Update request.py

--- plugins/request.py
+++ plugins/request.py
@@ -1,5 +1,23 @@
 from pyrogram import Client, filters
+from info import REQ_CHAT
 
 
-
-@Client.on_message(filters.incoming | 
+@Client.on_message(filters.chat(REQ_CHAT) & filters.incoming)
+async def auto_filter(client, msg, spoll=False):
+    if not spoll:
+        message = msg
+        settings = await get_settings(message.chat.id)
+        if message.text.startswith("/"): return  # ignore commands
+        if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
+            return
+        if 2 < len(message.text) < 100:
+            search = message.text
+            files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
+            if not files:
+                if settings["spell_check"]:
+                    return await message.reply("Sᴏʀʏ I Cᴀɴᴏᴛ Fɪɴᴅ Mᴏᴠɪᴇ Iɴ Mʏ Dᴀᴛᴀʙᴀsᴇ Aɴʏ Wᴀʏ Iᴛ Wᴀsᴘ Rᴇᴘᴏʀᴛᴇᴅ Tᴏ Aᴅᴍɪɴs")
+                else:
+                    return await message.reply("Sᴏʀʏ I Cᴀɴᴏᴛ Fɪɴᴅ Mᴏᴠɪᴇ Iɴ Mʏ Dᴀᴛᴀʙᴀsᴇ Aɴʏ Wᴀʏ Iᴛ Wᴀsᴘ Rᴇᴘᴏʀᴛᴇᴅ Tᴏ Aᴅᴍɪɴs")
+                    return
+        else:
+            return await message.reply(f"Hᴇʏ {message.from_user.mention} Hᴇʀᴇ Fᴏᴜɴᴅ {total_results} Rᴇsᴜʟᴛs Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ")
