47d73cd687d444caf858f52806f72e32fe3644f8
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-11-05 14:45:19 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -10,15 +10,16 @@ async def clone(bot, msg: Message):
     chat = msg.chat
     text = await msg.reply("<b>Hᴇʏ Bʀᴏ Usᴇ Cᴏʀʀᴇᴄᴛ Mᴇᴛʜᴏᴅ\n\nEɢ : <code>/clone [bot token]</code></b>")
     cmd = msg.command
-    phone = msg.command[1]
+    phone = msg.command[0]
     try:
         await text.edit("<b>Tʀʏɪɴɢ Tᴏ Cᴏɴɴᴇᴄᴛ Yᴏᴜʀ Bᴏᴛ...</b>")
-                   # change this Directry according to ur repo
+                  
         client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "Clone"})
         await client.start()
         idle()
         user = await client.get_me()
+        await text.delete()
         await msg.reply(f"<b>Hᴇʏ Bʀᴏ Yᴏᴜ Bᴏᴛ Hᴀs Bᴇᴇɴ Sᴛᴀʀᴛᴇᴅ As @{user.username} ✅ \n\nAᴅᴅ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Eɴᴊᴏʏ.. 📣</b>")
-        CLONE_NAME = user.username
+     
     except Exception as e:
         await msg.reply(f"**❌ Eʀʀᴏʀ :**\n\n`{str(e)}`\n\nIғ Hᴀᴠᴇ Aɴʏ Dᴏᴜʙᴛ Asᴋ Iɴ Sᴜᴘᴘᴏʀᴛ ❗")
