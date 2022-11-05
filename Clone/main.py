9c3bda7309e4903f75a278d5784a3df1a1f816b4
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-11-05 11:06:28 +0530

    Update main.py

--- Clone/main.py
+++ Clone/main.py
@@ -3,12 +3,12 @@ from pyrogram.types import *
 from plugins.clone import client
 @Client.on_message(filters.command(['start']) & filters.private)
 async def clone_start(bot, msg):
-  me = await msg.Client.get_me()
+ 
   btn = [[
       InlineKeyboardButton('❗Hᴇʟᴘ', callback_data="help"),
       InlineKeyboardButton('😎 Aʙᴏᴜᴛ', callback_data="about")
   ]]
   await msg.reply_text(
-      text = f"<b>Yᴏ Yᴏ !\nIᴀᴍ {me.username} Aɴ Sɪᴍᴘʟᴇ Aᴜᴛᴏ Fɪʟᴛᴇ + Fɪʟᴇ Sʜᴀʀᴇ Bᴏᴛ...</b>",
+      text = f"<b>Yᴏ Yᴏ !\nIᴀᴍ A Sɪᴍᴘʟᴇ Aᴜᴛᴏ Fɪʟᴛᴇ + Fɪʟᴇ Sʜᴀʀᴇ Bᴏᴛ...</b>",
       reply_markup = InlineKeyboardMarkup(btn)
   )
