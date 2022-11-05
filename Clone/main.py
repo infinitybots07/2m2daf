5e8e43eac78d0027682e8cda1f2d4821e800bf04
nihaal-new <111719488+nihaal-new@users.noreply.github.com>
2022-11-05 22:05:03 +0530

    Update main.py

--- Clone/main.py
+++ Clone/main.py
@@ -1,6 +1,39 @@
+
+import psutil
+import asyncio
+import re
+import ast
+import random
+import datetime
+import pytz
+import math
+from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+from Script import script
+import pyrogram
+from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
+    make_inactive
+from info import ADMINS, PICS_RT, AUTH_CHANNEL, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, IMDB, \
+    SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE, DELETE_TIME, CH_FILTER, CH_LINK, UNAUTHORIZED_CALLBACK_TEXT, REQ_PIC, \
+    AUTO_FILTER
 from pyrogram import *
 from pyrogram.types import *
+from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
+from utils import get_size, get_poster, search_gagala, temp, get_settings, save_group_settings
+from database.users_chats_db import db
+from database.ia_filterdb import Media, get_file_details, get_search_results
+from database.filters_mdb import (
+    filter_stats,
+    del_all,
+    find_filter,
+    get_filters,
+)
+import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+BUTTONS = {}
+SPELL_CHECK = {}
 
 @Client.on_message(filters.command(['start']) & filters.private)
 async def clone_start(bot, msg):
