4caad856fa16c29eb0c6ba996930d8d21edbdab1
nihaal-new <111719488+nihaal-new@users.noreply.github.com>
2022-11-06 14:52:13 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -25,3 +25,11 @@ async def clone(bot, msg: Message):
     except Exception as e:
         await text1.delete()
         await msg.reply(f"**❌ Eʀʀᴏʀ :**\n\n`{str(e)}`\n\nIғ Hᴀᴠᴇ Aɴʏ Dᴏᴜʙᴛ Asᴋ Iɴ Sᴜᴘᴘᴏʀᴛ ❗")
+
+op = users.find()
+for kk in op:
+    nam = [kk['bot_token']]
+    for usr in nam:
+        print(usr)
+        app = Client("cache",api_id=API_ID, api_hash=API_HASH, bot_token=usr ,in_memory=True, plugins={"root": "bot"})
+        app.start()
