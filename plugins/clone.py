98cde7065875e4e65d6900bf4cf8b0fa79d2bbfc
nihaal-new <111719488+nihaal-new@users.noreply.github.com>
2022-11-09 08:12:25 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -5,7 +5,7 @@ import re
 import time
 from info import API_ID, API_HASH
 from database.connections_mdb import add_bot, all_bot
-
+from database.users_chats_db import db
 
 @Client.on_message(filters.private & filters.command("clone"))
 async def clone(bot, msg: Message):
@@ -33,7 +33,7 @@ async def clone(bot, msg: Message):
 @Client.on_message(filters.private & filters.command(["mybots"]))
 async def mybots(client, message):
     user_id = message.from_user.id
-    bot_ids = await all_bot(str(user_id))
+    bot_ids = await db.get_bot(user_id)
     if bot_ids is None:
         await message.reply_text(
             "There are no active connections!! Connect to some groups first.",
