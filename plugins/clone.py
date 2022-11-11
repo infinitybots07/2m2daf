d45dc9c4aee30f195dfd2afcfa2a5abc69ccf8f9
nihaal-new <111719488+nihaal-new@users.noreply.github.com>
2022-11-11 07:37:44 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -11,10 +11,10 @@ from database.users_chats_db import db
 @Client.on_message(filters.private & filters.command("clone") & ~filters.bot, group=3)
 async def clone(bot:Client, msg:Message):
     chat = msg.chat
-    post:Message = await bot.ask(chat_id=msg.from_user.id, text = "Oᴋᴀʏ Nᴏᴡ Sᴇɴᴛ Mᴇ Bᴏᴛ Tᴏᴋᴇɴ", timeout = 360)
+    post:Message = await Client.ask(chat_id=msg.from_user.id, text = "Oᴋᴀʏ Nᴏᴡ Sᴇɴᴛ Mᴇ Bᴏᴛ Tᴏᴋᴇɴ", timeout = 360)
     phone = post.command[1]
     cmd = msg.command
-    bot_id1 = phone.text.split(":")[0]
+    bot_id1 = post.text.split(":")[0]
     try:
         text1 = await msg.reply("<b>Tʀʏɪɴɢ Tᴏ Cᴏɴɴᴇᴄᴛ Yᴏᴜʀ Bᴏᴛ...</b>")
                   
