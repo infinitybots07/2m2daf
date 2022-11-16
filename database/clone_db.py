fdcc790f2f06410b5f8f44d7d8a8b76058c747a3
nihaal-new <111719488+nihaal-new@users.noreply.github.com>
2022-11-16 13:58:14 +0530

    Update clone_db.py

--- database/clone_db.py
+++ database/clone_db.py
@@ -23,7 +23,7 @@ async def rm_all_session():
     await string.delete_many({})
 
 async def get_all_bot():
-    lol = await string.find({})
+    lol = [n async for n in string.find({})]
     return lol
 
 async def is_session_in_db(client):
