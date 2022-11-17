21b108e0d2e20ee80c75c15aa596e706df1af97c
nihaal-new <111719488+nihaal-new@users.noreply.github.com>
2022-11-17 21:06:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,50 +14,39 @@ from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
-from database.clone_db import get_all_bot
 import pyromod.listen
 
+class Bot(Client):
 
-bot = Client(
-    name=SESSION,
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    workers=200,
-    plugins={"root": "plugins"},
-    sleep_threshold=10
-)
+    def __init__(self):
+        super().__init__(
+            name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=100,
+            plugins={"root": "plugins"},
+            sleep_threshold=10,
+        )
+
+    async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
+        await super().start()
+        await Media.ensure_indexes()
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
+
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot stopped. Bye.")
 
 
-async def start_bot():
-    b_users, b_chats = await db.get_banned()
-    temp.BANNED_USERS = b_users
-    temp.BANNED_CHATS = b_chats
-    await bot.start()
-    await Media.ensure_indexes()
-    me = await bot.get_me()
-    temp.ME = me.id
-    temp.U_NAME = me.username
-    temp.B_NAME = me.first_name
-    bot.username = '@' + me.username
-    print("[INFO]: LOADING ASSISTANT DETAILS")
-    string = await get_all_bot()
-    bot_id = await string.text.split(":")[0]
-    for i in string:
-        try:
-            pyroman = Client(bot_id + "_0", api_id=API_ID, api_hash=API_HASH, bot_token=string, plugins={"root": "Clone"})
-            await pyroman.start()                                                                                                       
-            user = await pyroman.get_me()
-            temp.CL = user.id
-            temp.C_NAME = user.username
-            print(f"[INFO]: Started {user.first_name}")
-        except BaseException as eb:
-            print(eb)
-    print(f"Total Client = {len(string)} User")
-    logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-    logging.info(LOG_STR)
-    
-    
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_bot())
-
+app = Bot()
+app.run()
